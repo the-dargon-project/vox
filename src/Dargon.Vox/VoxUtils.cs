e30a5e6e2f1d95ae2aea26ebd030d59aa7fb45af
Warty <ItzWarty@gmail.com>
2016-06-08 07:53:52 -0700

    wip

---
+++ src/Dargon.Vox/VoxUtils.cs
@@ -0,0 +1,19 @@
+﻿using Dargon.Commons.Pooling;
+using System.IO;
+
+namespace Dargon.Vox {
+   public static class VoxUtils {
+      private static readonly IObjectPool<MemoryStream> memoryStreamPool = ObjectPool.Create(() => new MemoryStream());
+
+      public static T DeepCloneSerializable<T>(this T x) {
+         var ms = memoryStreamPool.TakeObject();
+         try {
+            Serialize.To(ms, x);
+            ms.Position = 0;
+            return Deserialize.From<T>(ms);
+         } finally {
+            memoryStreamPool.ReturnObject(ms);
+         }
+      }
+   }
+}
